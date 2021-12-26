912cc531a2c0cf082fe37a18ada756206ddbc3f6
MartinaB91 <martinabjorlin@hotmail.com>
2021-12-26 14:59:25 +0000

    Moved changing of current player to own function changePlayer.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,7 @@ let currentPlayer = "not assigned";
 
 // Add eventlistener first when DOM is finish loading. 
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByClassName('tile-btn')
+    let buttons = document.getElementsByClassName('tile-btn');
 
     for (button of buttons) {
         button.addEventListener('click', gameBoardBtnClick);
@@ -16,19 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function gameBoardBtnClick () {
     let button = this;
     setPlayerOnBtn(button);
-}
-
-// Using the global variable currentPlayer to set the player on the button. 
-function setPlayerOnBtn(button) {
-
-    if (currentPlayer === 'X') {
-        button.innerHTML = 'X';
-        currentPlayer = 'O';
-    } else {
-        button.innerHTML = 'O';
-        currentPlayer = 'X';
-    }
-
+    checkIfWinn();
+    changePlayer();
 }
 
 // Randomize which player is first out to play X or O. 
@@ -36,5 +25,47 @@ function getRandomPlayer() {
     const players = ['X', 'O'];
     const randomPlayer = Math.floor(Math.random() * players.length);
     currentPlayer = (players[randomPlayer]);
+}
+
+// Using the global variable currentPlayer to set the player on the button. 
+function setPlayerOnBtn(button) {
+
+    if (currentPlayer === 'X') {
+        button.innerHTML = 'X';
+        // currentPlayer = 'O';
+    } else {
+        button.innerHTML = 'O';
+        // currentPlayer = 'X';
+    }
+
+}
+function changePlayer() {
+
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+
+}
+
+function checkIfWinn() {
+    let btn1 = document.getElementById('btn-tile-1');
+    let btn2 = document.getElementById('btn-tile-2');
+    let btn3 = document.getElementById('btn-tile-3');
+    let btn4 = document.getElementById('btn-tile-4');
+    let btn5 = document.getElementById('btn-tile-5');
+    let btn6 = document.getElementById('btn-tile-6');
+    let btn7 = document.getElementById('btn-tile-7');
+    let btn8 = document.getElementById('btn-tile-8');
+    let btn9 = document.getElementById('btn-tile-9');
+
+    function winnScenarios(btnA, btnB, btnC) {
+        if (btnA.innerHTML === 'X' && btnB.innerHTML === 'X' && btnC.innerHTML === 'X') {
+            alert('Vinst');
+        }
+    }
+
+    winnScenarios(btn1, btn2, btn3);
 }
 

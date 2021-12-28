e213d4e3035065c366de0edf3ae2e0f73423c6cf
MartinaB91 <martinabjorlin@hotmail.com>
2021-12-28 17:31:19 +0000

    Added html for next player and function writeOutPlayer for writing out who's turn it is.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,13 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initialize currentPlayer.
     getRandomPlayer();
 
+    // Shows witch player starts playing. 
+    let nextPlayer = document.getElementById('player');
+    nextPlayer.innerHTML = currentPlayer;
+    
 });
 
 /**Main function that calls other functions and desides the order they are executed in. 
  * Main function is called every time a button on game board is clicked. */
 function gameBoardBtnClick() {
     let button = this;
-    button.disabled = true; 
+    button.disabled = true;
+    writeOutPlayer(); // Writes out who is next in line
     setPlayerOnBtn(button) // Set player X/O on the button
     checkIfWin(); // Checks if someone has won or if the game is draw
     changePlayer(); // Change player
@@ -37,16 +42,15 @@ function getRandomPlayer() {
 
 // Using the global variable currentPlayer to set the player on the button. 
 function setPlayerOnBtn(button) {
-
     if (currentPlayer === 'X') {
         button.innerHTML = 'X';
     } else {
         button.innerHTML = 'O';
     }
 }
+
 // Changes who plays
 function changePlayer() {
-
     if (currentPlayer === 'X') {
         currentPlayer = 'O';
     } else {
@@ -70,30 +74,30 @@ function checkIfWin() {
         if (btnA.innerHTML === currentPlayer && btnB.innerHTML === currentPlayer && btnC.innerHTML === currentPlayer) {
             alert(`Congratulations player ${currentPlayer} you won!`);
             restartGame();
-            return 1;  
-        } 
+            return 1;
+        }
         return 0;
     }
 
     let winCount = 0;
 
-   winCount += winScenarios(btn1, btn2, btn3); // First row horizontal
-   winCount += winScenarios(btn4, btn5, btn6); // Second row horizontal
-   winCount += winScenarios(btn7, btn8, btn9); // Third row horizontal
-   winCount += winScenarios(btn1, btn4, btn7); // First row vertical 
-   winCount += winScenarios(btn2, btn5, btn8); // Second row vertical
-   winCount += winScenarios(btn3, btn6, btn9); // Second row vertical
-   winCount += winScenarios(btn3, btn5, btn7); // Diagonally right to bottom
-   winCount += winScenarios(btn1, btn5, btn9); // Diagonally left to bottom
+    winCount += winScenarios(btn1, btn2, btn3); // First row horizontal
+    winCount += winScenarios(btn4, btn5, btn6); // Second row horizontal
+    winCount += winScenarios(btn7, btn8, btn9); // Third row horizontal
+    winCount += winScenarios(btn1, btn4, btn7); // First row vertical 
+    winCount += winScenarios(btn2, btn5, btn8); // Second row vertical
+    winCount += winScenarios(btn3, btn6, btn9); // Second row vertical
+    winCount += winScenarios(btn3, btn5, btn7); // Diagonally right to bottom
+    winCount += winScenarios(btn1, btn5, btn9); // Diagonally left to bottom
 
-   /**To prevent draw happening if someone wins at the last turn.
-    * The checkIfDraw function is only called when someone hasen't won  */
+    /**To prevent draw happening if someone wins at the last turn.
+     * The checkIfDraw function is only called when someone hasen't won  */
     if (winCount === 0) {
         checkIfDraw();
     }
 }
 
-//Hides the start button and make the buttons clickable.
+// Hides the start button and make the buttons clickable.
 function startGame() {
     document.getElementById('start-btn').style.display = "none";
     let buttons = document.getElementsByClassName('tile-btn');
@@ -114,16 +118,15 @@ function restartGame() {
         button.disabled = true;
         button.innerHTML = "";
     }
-
 }
+
 // Checks if every button is claimed. If they are the result is draw. 
 function checkIfDraw() {
-
     let buttons = document.getElementsByClassName('tile-btn');
     let countClickedButtons = 0;
 
     for (let button of buttons) {
-        
+
         if (button.disabled === true) {
             countClickedButtons++;
         }
@@ -133,5 +136,13 @@ function checkIfDraw() {
         alert('Draw!');
         restartGame();
     }
-
+}
+// Write out whose turn it is. 
+function writeOutPlayer() {
+    let nextPlayer = document.getElementById('player');
+    if (currentPlayer === 'X') {
+        nextPlayer.innerHTML = 'O';
+    } else {
+        nextPlayer.innerHTML = 'X';
+    }
 }
